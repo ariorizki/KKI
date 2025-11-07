9f08abc1ca9be9a642299b562a76e84ef9b0a1fc
PM MSTT <PM MSTT@MSTTPM>
2025-11-07 18:01:51 +0700

    test commit auto deploy 1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,6 +23,8 @@ function App() {
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
+
+          WEBSITE INI UNTUK PROJECT KKI
         </p>
       </div>
       <p className="read-the-docs">
